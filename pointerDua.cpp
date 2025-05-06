f1ccef3a0fc5160a404dab550ad4faf522be5f84
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-05-06 18:57:37 +0700

    Menambahkan fungsi dan isi int main Pointer Object mhs

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -9,9 +9,14 @@ class mahasiswa
         {
             cout << "No Induk = "<< nim << endl;
         }
-}
+};
+
 
 int main ()
 {
-
+    mahasiswa *mhs = new mahasiswa{1}; //Pointer Object mhs
+    mhs ->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
 }
