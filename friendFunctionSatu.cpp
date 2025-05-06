6c199a4dad20ccc0e2af109b94356dff5dbfe7b1
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-05-06 19:01:36 +0700

    menambahkan void untuk setNama

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 
 
-clas mahasiswa 
+class mahasiswa 
 {
     private :
         string nama;
     public : 
         friend void setNama(mahasiswa &a, string);
 };
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
 
 int main ()
 {
