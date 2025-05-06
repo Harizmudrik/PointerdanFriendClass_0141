2eb9981f5e3517acd4e8fc156aacbfc39939835b
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-05-06 18:24:33 +0700

    Membuat void input yang public dari panjang dan lebar

--- contoh.cpp
+++ contoh.cpp
@@ -6,7 +6,14 @@ class PersegiPanjang
     public: //akses modifier
     int panjang, lebar;
 
-
+    public:
+        void input()
+        {
+            cout << "Masukkan Panjang : ";
+            cin >> panjang;
+            cout << "Masukkan Lebar : ";
+            cin >> lebar;
+        }
 }
 
 
