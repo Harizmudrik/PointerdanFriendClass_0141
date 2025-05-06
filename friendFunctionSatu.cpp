2ee7d66b719449aa2279b554ffe581b3df43b877
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-05-06 19:00:31 +0700

    Membuat class mahasiswa dnegan type private dan public

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -2,8 +2,17 @@
 #include <string>
 using namespace std;
 
+
+clas mahasiswa 
+{
+    private :
+        string nama;
+    public : 
+        friend void setNama(mahasiswa &a, string);
+};
+
 int main ()
 {
-    
+
 }
 
